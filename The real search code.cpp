54b87b6f05ae0b2652a772e1dc6413cb653925e5
Andhika Akbar Pratama <q4quekar@gmail.com>
2024-10-08 21:58:13 +0700

    Update The real search code.cpp
    menambah awalan

--- The real search code.cpp
+++ The real search code.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
+int angkaRand1(int rentang) {
+    srand(time(0));
+    return (rand() / 10 * 25) % rentang;
+}
+
+int angkaRand2(int rentang) {
+    srand(time(0));
+    return (rand() / 10 * 13) % rentang;
+}
+
+int main(){
+    string user;
+    long int NPM;
+    int randSoal, mode, soal = 10;
+    long double key, jawaban;
+    int poin = 100;
+    
+    cout << "=== Welcome to brain teasers and accuracy games ===" << endl 
+         << "Input Username: ";
+     getline(cin, user);
+    cout << "Input Your NPM: ";
+    cin >> NPM;
